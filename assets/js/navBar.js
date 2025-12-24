e1e4586d027e3050396edc96da8edd2fd7f6c8b8
ParthibanR <parthibanvijay467@gmail.com>
2025-12-24 22:00:57 +0530

    MODIFIED:R#

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -1,34 +1,56 @@
 $(document).ready(function () {
-    $('#home').click(function(){
-        $('.cricket_News').hide();
-        $('.ipl_Special').hide();
-        $('.match_Analysis').hide();
-        $('.home_Content').show().load('homeBlog.html');
-      });
-      
-      $('#cricketNews').click(function(){
-        $('.home_Content').hide();
-        $('.ipl_Special').hide();
-        $('.match_Analysis').hide();
-        $('.cricket_News').show().load('cricketNews.html');
-      });
-      
-      $('#analysis').on('click',function(){
-        $('.home_Content').hide();
-        $('.cricket_News').hide();
-        $('.ipl_Special').hide();
-        $('.match_Analysis').show().load('matchAnalysis.html')
-      });
+  $('#home').click(function () {
+    $('.cricket_News').hide();
+    $('.ipl_Special').hide();
+    $('.match_Analysis').hide();
+    $('.home_Content').show().load('homeBlog.html');
+  });
 
-      $('#special').on('click',function(){
-        $('.home_Content').hide();
-        $('.cricket_News').hide();
-        $('.match_Analysis').hide();
-        $('.ipl_Special').show().load('iplSpecial.html')
-      });
+  $('#cricketNews').click(function () {
+    $('.home_Content').hide();
+    $('.ipl_Special').hide();
+    $('.match_Analysis').hide();
+    $('.cricket_News').show().load('cricketNews.html');
+  });
 
-      $('.home_Content').load('homeBlog.html');
-      $('.cricket_News').load('cricketNews.html');
-      $('.ipl_Special').load('iplSpecial.html');
-      $('.match_Analysis').load('matchAnalysis.html');
+  $('#analysis').on('click', function () {
+    $('.home_Content').hide();
+    $('.cricket_News').hide();
+    $('.ipl_Special').hide();
+    $('.match_Analysis').show().load('matchAnalysis.html')
+  });
+
+  $('#special').on('click', function () {
+    $('.home_Content').hide();
+    $('.cricket_News').hide();
+    $('.match_Analysis').hide();
+    $('.ipl_Special').show().load('iplSpecial.html')
+  });
+
+  $('.home_Content').load('homeBlog.html');
+  $('.cricket_News').load('cricketNews.html');
+  $('.ipl_Special').load('iplSpecial.html');
+  $('.match_Analysis').load('matchAnalysis.html');
+
+  $('.footer').css({
+    'text-align': 'center',
+    'padding': '10px 0',
+    'background-color': '#333',
+    'color': '#fff',
+    'font-size': '14px',
+    'margin-top': '20px'
+  });
+
+  let Years = new Date().getFullYear();
+  $('.footer span').append(Years);
+
+  $('.footer a').css({
+    'color': '#ff9800',
+    'text-decoration': 'none',
+    'margin': '0 5px'
+  }).hover(function () {
+    $(this).css('text-decoration', 'underline');
+  }, function () {
+    $(this).css('text-decoration', 'none');
+  });
 });
